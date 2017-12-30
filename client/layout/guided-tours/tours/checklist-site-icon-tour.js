57ce550f9dda3caa1ab9c493ee64b63dea6429dd
Alex Kirk <alex.git@kirk.at>
2017-12-30 10:39:47 +0100

    Add more button labels

--- client/layout/guided-tours/tours/checklist-site-icon-tour.js
+++ client/layout/guided-tours/tours/checklist-site-icon-tour.js
@@ -22,6 +22,10 @@ import {
 	Tour,
 } from 'layout/guided-tours/config-elements';
 
+const ChangeButtonLabel = translate( 'Change' );
+const ContinueButtonLabel = translate( 'Continue' );
+const DoneButtonLabel = translate( 'Done' );
+
 export const ChecklistSiteIconTour = makeTour(
 	<Tour name="checklistSiteIcon" version="20171205" path="/non-existent-route" when={ noop }>
 		<Step
@@ -35,7 +39,10 @@ export const ChecklistSiteIconTour = makeTour(
 		>
 			<p>
 				{ translate(
-					'Press this graphic to upload your own image or icon that can help people identify your site in the browser.'
+					'Press {{ChangeButton/}} to upload your own image or icon that can help people identify your site in the browser.',
+					{
+						components: { ChangeButton: <strong>{ ChangeButtonLabel }</strong> },
+					}
 				) }
 			</p>
 			<ButtonRow>
@@ -65,8 +72,8 @@ export const ChecklistSiteIconTour = makeTour(
 			placement="above"
 		>
 			<Continue target="dialog-base-action-confirm" step="click-done" click>
-				{ translate( 'Good choice, press {{b}}Continue{{/b}} to use it as your Site Icon.', {
-					components: { b: <strong /> },
+				{ translate( 'Good choice, press {{ContinueButton/}} to use it as your Site Icon.', {
+					components: { ContinueButton: <strong>{ ContinueButtonLabel }</strong> },
 				} ) }
 			</Continue>
 		</Step>
@@ -79,8 +86,8 @@ export const ChecklistSiteIconTour = makeTour(
 		>
 			<Continue target="image_editor_button_done" step="finish" click>
 				{ translate(
-					'Let’s make sure it looks right before you press {{b}}Done{{/b}} to save your changes.',
-					{ components: { b: <strong /> } }
+					'Let’s make sure it looks right before you press {{DoneButton/}} to save your changes.',
+					{ components: { DoneButton: <strong>{ DoneButtonLabel }</strong> } }
 				) }
 			</Continue>
 		</Step>
