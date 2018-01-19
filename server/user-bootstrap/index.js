619e70ade27cf767b5c4c171da17b5604ce58bc6
Jason Moon <jason.moon@automattic.com>
2018-01-19 16:55:27 -0700

    [bootstrap] Refactor cookie binding to its own fn

--- server/user-bootstrap/index.js
+++ server/user-bootstrap/index.js
@@ -12,26 +12,29 @@ const AUTH_COOKIE_NAME = 'wordpress_logged_in';
  * WordPress.com REST API /me endpoint.
  */
 const url = 'https://public-api.wordpress.com/rest/v1/me?meta=flags';
+function bindAuthorizationToRequest( request, authCookieValue, callback ) {
+	authCookieValue = decodeURIComponent( authCookieValue );
+
+	if ( typeof API_KEY !== 'string' ) {
+		callback( new Error( 'Unable to boostrap user because of invalid API key in secrets.json' ) );
+		return;
+	}
+
+	const hmac = crypto.createHmac( 'md5', API_KEY );
+	hmac.update( authCookieValue );
+	const hash = hmac.digest( 'hex' );
+
+	request.set( 'Authorization', 'X-WPCALYPSO ' + hash );
+	request.set( 'Cookie', AUTH_COOKIE_NAME + '=' + authCookieValue );
+	request.set( 'User-Agent', 'WordPress.com Calypso' );
+}
 
 module.exports = function( authCookieValue, callback ) {
 	// create HTTP Request object
 	const req = superagent.get( url );
 
 	if ( authCookieValue ) {
-		authCookieValue = decodeURIComponent( authCookieValue );
-
-		if ( typeof API_KEY !== 'string' ) {
-			callback( new Error( 'Unable to boostrap user because of invalid API key in secrets.json' ) );
-			return;
-		}
-
-		const hmac = crypto.createHmac( 'md5', API_KEY );
-		hmac.update( authCookieValue );
-		const hash = hmac.digest( 'hex' );
-
-		req.set( 'Authorization', 'X-WPCALYPSO ' + hash );
-		req.set( 'Cookie', AUTH_COOKIE_NAME + '=' + authCookieValue );
-		req.set( 'User-Agent', 'WordPress.com Calypso' );
+		bindAuthorizationToRequest( req, authCookieValue, callback );
 	}
 
 	// start the request
