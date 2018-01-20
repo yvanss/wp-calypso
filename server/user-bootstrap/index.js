06098c0a8f0a2f25e0c0c19ff3a5217fab4a7bd5
Jason Moon <jason.moon@automattic.com>
2018-01-19 17:11:25 -0700

    [bootstrap] Add feature-gated variation fetching

--- server/user-bootstrap/index.js
+++ server/user-bootstrap/index.js
@@ -6,12 +6,19 @@ const crypto = require( 'crypto' );
 const config = require( 'config' );
 const API_KEY = config( 'wpcom_calypso_rest_api_key' );
 const userUtils = require( './shared-utils' );
+const activeTests = require( 'client/lib/abtest/active-tests' );
 const AUTH_COOKIE_NAME = 'wordpress_logged_in';
 
 /**
  * WordPress.com REST API /me endpoint.
  */
 const url = 'https://public-api.wordpress.com/rest/v1/me?meta=flags';
+const abVariationsUrl = 'https://public-api.wordpress.com/rest/v1.1/me/abtests';
+
+function getTestNames() {
+	return Object.keys( activeTests ).map( key => `${ key }_${ activeTests[ key ].datestamp }` );
+}
+
 function bindAuthorizationToRequest( request, authCookieValue, callback ) {
 	authCookieValue = decodeURIComponent( authCookieValue );
 
@@ -24,8 +31,8 @@ function bindAuthorizationToRequest( request, authCookieValue, callback ) {
 	hmac.update( authCookieValue );
 	const hash = hmac.digest( 'hex' );
 
-	request.set( 'Authorization', 'X-WPCALYPSO ' + hash );
-	request.set( 'Cookie', AUTH_COOKIE_NAME + '=' + authCookieValue );
+	request.set( 'Authorization', `X-WPCALYPSO ${ hash }` );
+	request.set( 'Cookie', `${ AUTH_COOKIE_NAME }=${ authCookieValue }` );
 	request.set( 'User-Agent', 'WordPress.com Calypso' );
 }
 
@@ -58,6 +65,33 @@ module.exports = function( authCookieValue, callback ) {
 		}
 
 		const user = userUtils.filterUserObject( body );
-		callback( null, user );
+		if ( config.isEnabled( 'wpcom-user-bootstrap-ab-variations' ) && user.ID ) {
+			/* NOTE: Currently doesn't work as intended due to authorization issues and is thus feature gated.
+			 * Error body:
+				{
+					"error":"unauthorized",
+					"message":"That API call is not allowed for this account."
+				}
+			*/
+			debug( 'Fetching A/B test variation assignment for user' );
+			const variationReq = superagent.get( abVariationsUrl, getTestNames() );
+			if ( authCookieValue ) {
+				bindAuthorizationToRequest( variationReq, authCookieValue, callback );
+			}
+			variationReq.end( function( variationErr, variationResponse ) {
+				if ( variationErr ) {
+					// Skip trying to fetch variation data
+					debug( 'Failed to fetch variation data', variationErr );
+					return callback( null, user );
+				}
+				const { body: variationBody, status: variationStatusCode } = variationResponse;
+				debug( '%o -> %o status code', abVariationsUrl, variationStatusCode );
+
+				user.abtests = variationBody;
+				callback( null, user );
+			} );
+		} else {
+			callback( null, user );
+		}
 	} );
 };
